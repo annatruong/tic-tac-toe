4a8a4a9ed0c838fffb9c7265fb05268eb8ce15fe
Anna Truong <annatruong321@hotmail.com>
2024-05-20 10:35:27 +0100

    added game intro

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -4,6 +4,25 @@
 
 int main()
 {
+    // Print instructions
+    std::cout << "                                                                                                                                                \n";
+    std::cout << "________________________________________________________________________________________________________________________________________________\n";
+    std::cout << "________________________________________________________________________________________________________________________________________________\n";
+    std::cout << "                                                                                                                                                \n";
+    std::cout << "     OOOOOOOOOOOOOO  XXXX      OOOOOOOOO      XXXXXXXXXXXXXX   OOOOOOOOOO      XXXXXXXXX      OOOOOOOOOOOOOO     XXXXXXXXX     OOOOOOOOOOOO     \n";
+    std::cout << "     OOOOOOOOOOOOOO  XXXX    OOOOOOOOOOOOO    XXXXXXXXXXXXXX  OOOOOOOOOOOO   XXXXXXXXXXXXX    OOOOOOOOOOOOOO   XXXXXXXXXXXXX   OOOOOOOOOOOO     \n";
+    std::cout << "          OOOO       XXXX   OOOOO                  XXXX       OOOO    OOOO  XXXXX                  OOOO       XXXXX     XXXXX  OOOO             \n";
+    std::cout << "          OOOO       XXXX   OOOOO                  XXXX       OOOOOOOOOOOO  XXXXX                  OOOO       XXXXX     XXXXX  OOOOOOOOOOOO     \n";
+    std::cout << "          OOOO       XXXX   OOOOO                  XXXX       OOOOOOOOOOOO  XXXXX                  OOOO       XXXXX     XXXXX  OOOO             \n";
+    std::cout << "          OOOO       XXXX    OOOOOOOOOOOOO         XXXX       OOOO    OOOO   XXXXXXXXXXXXX         OOOO        XXXXXXXXXXXXX   OOOOOOOOOOOO     \n";
+    std::cout << "          OOOO       XXXX      OOOOOOOOO           XXXX       OOOO    OOOO     XXXXXXXXX           OOOO          XXXXXXXXX     OOOOOOOOOOOO     \n";
+    std::cout << "                                                                                                                                                \n";
+    std::cout << "________________________________________________________________________________________________________________________________________________\n";
+    std::cout << "________________________________________________________________________________________________________________________________________________\n";
+    std::cout << "                                                                                                                                                \n";    
+    std::cout << "- Player 1 is represented by Os and Player 2 is represented by Xs.\n";
+    std::cout << "- Players take turns putting their marks in empty squares.\n";
+    std::cout << "- The first player to get 3 marks in a row (up, down, across, or diagonally) is the winner.\n\n";
     // Initialize the grid
     std::vector<std::vector<std::string>> grid(3, std::vector<std::string>(3, " "));
 
--- tic_tac_toe_fn.cpp
+++ tic_tac_toe_fn.cpp
@@ -13,26 +13,22 @@ std::vector<std::vector<std::string> > getNextMove(int player, std::vector<std::
         std::cout << "Player " << player << ", please enter where you would like to place your next move (row column (1 - 3)): \n";
         std::cin >> row >> column;
 
-        // check if player has entered current input
+        // check if player has entered correct input
         if(row < 1 || row > 3 || column < 1 || column > 3) {
-            std::cout << "Incorrect row/column value. Please enter a number between 1 - 3 inclusively.\n\n";
+            std::cout << "\nIncorrect row/column value. Please enter a number between 1 - 3 inclusively.\n\n";
         } else {
             std::string current_value = grid[row - 1][column - 1];
             // check if position already taken
-            if(current_value == "O" || current_value == "X") {
-                std::cout << "Position taken. Please enter a different position.\n\n";
-            } else {
-                correct_input = true;
-            }
+            if(current_value == "O" || current_value == "X") std::cout << "Position taken. Please enter a different position.\n\n";
+             else correct_input = true;
         }
 
     }
 
-    if (player == 1) {
-        grid[row - 1][column - 1] = 'O';
-    } else {
-        grid[row - 1][column - 1] = 'X';
-    }
+    // add move to grid
+    if (player == 1) grid[row - 1][column - 1] = 'O';
+    else grid[row - 1][column - 1] = 'X';
+
     return grid;
 }
 
@@ -41,7 +37,6 @@ std::vector<std::vector<std::string> > getNextMove(int player, std::vector<std::
  * Check if there is a winner
  * */
 bool findWinner(std::vector<std::vector<std::string> > grid) {
-    // check if these is a winner
     if(
     (grid[0][0] == "O" && grid[0][1] == "O" && grid[0][2] == "O") || (grid[0][0] == "X" && grid[0][1] == "X" && grid[0][2] == "X") ||
     (grid[1][0] == "O" && grid[1][1] == "O" && grid[1][2] == "O") || (grid[1][0] == "X" && grid[1][1] == "X" && grid[1][2] == "X") ||
@@ -63,7 +58,6 @@ bool findWinner(std::vector<std::vector<std::string> > grid) {
  * */
 bool checkDraw(std::vector<std::vector<std::string> > grid) {
 
-    // check if grid is full
     for (const auto& row : grid) {
         for (const auto& value : row) {
             if(value == " ") return false;
